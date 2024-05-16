e0a94ea3d7966c82ec342f9c1582ce6019ab1fd4
khai2007 <121837564+khai2007@users.noreply.github.com>
2024-05-16 13:27:36 +0700

    Add files via upload

---
+++ MilkShopSystemDatabase.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE MilkShop
+USE MilkShop
+DROP DATABASE MilkShop
+CREATE TABLE Users (
+    user_id INT IDENTITY(1,1) PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    role VARCHAR(10) CHECK (role IN ('guest', 'member', 'staff', 'admin')) NOT NULL,
+    points INT DEFAULT 0
+);
+
+CREATE TABLE Category (
+	category_id INT IDENTITY(1,1) PRIMARY KEY,
+	category_name nvarchar(50)
+);
+
+CREATE TABLE Products (
+    product_id INT IDENTITY(1,1) PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT NOT NULL,
+    category_id INT REFERENCES Category(category_id)
+);
+
+CREATE TABLE Reviews (
+    review_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT,
+    product_id INT,
+    rating INT CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    review_date DATETIME,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+CREATE TABLE Orders (
+    order_id INT IDENTITY(1,1) PRIMARY KEY,
+    user_id INT,
+    order_date DATETIME,
+    status VARCHAR(10) CHECK (status IN ('pending', 'confirmed', 'shipped', 'delivered', 'cancelled')),
+    total_amount DECIMAL(10, 2),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Order_Items (
+    order_item_id INT IDENTITY(1,1) PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10, 2),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+CREATE TABLE Vouchers (
+    voucher_id INT IDENTITY(1,1) PRIMARY KEY,
+    code VARCHAR(50) UNIQUE,
+    discount DECIMAL(5, 2),
+    expiration_date DATE
+);
+
+CREATE TABLE User_Vouchers (
+    user_id INT,
+    voucher_id INT,
+    used BIT DEFAULT 0,
+    PRIMARY KEY (user_id, voucher_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (voucher_id) REFERENCES Vouchers(voucher_id)
+);
+
+CREATE TABLE Payment_Methods (
+    payment_method_id INT IDENTITY(1,1) PRIMARY KEY,
+    method_name VARCHAR(50) NOT NULL,
+    details TEXT
+);
+
+CREATE TABLE Payments (
+    payment_id INT IDENTITY(1,1) PRIMARY KEY,
+    order_id INT,
+    payment_method_id INT,
+    transaction_date DATETIME NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    transaction_status VARCHAR(20) CHECK (transaction_status IN ('pending', 'completed', 'failed')) NOT NULL,
+    payment_details TEXT,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (payment_method_id) REFERENCES Payment_Methods(payment_method_id)
+);
+
