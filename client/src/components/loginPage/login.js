ff91b1c877c2dfc706ab420d9d977850ed14dbdb
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-05-22 08:45:57 +0700

    update layout login, register

--- client/src/components/loginPage/login.js
+++ client/src/components/loginPage/login.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react'
-import './login.scss'
+import React, { useState } from "react";
+import "./login.scss";
 import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { IoEyeSharp } from "react-icons/io5";
 
 function Login() {
-
-  const [userID, setUserID] = useState('');
-  const [password, setPassword] = useState('');
+  const [userID, setUserID] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
   const handleOnchangeUserID = (event) => {
@@ -20,54 +19,57 @@ function Login() {
   };
 
   const handleShowPass = () => {
-    setShowPassword(!showPassword)
-  }
+    setShowPassword(!showPassword);
+  };
 
   return (
     <>
-      <div className='container'>
-        <div className='column'>
+      <div className="login_container">
+        <div className="column">
           <h2>Log In</h2>
 
           <form>
-            <div className='login_info'>
-              <div className='login_detail'>
+            <div className="login_info">
+              <div className="login_detail">
                 <FaUser />
-                <input type='text' value={userID} placeholder='Email or Phone Number'
-                  onChange={handleOnchangeUserID} required />
+                <input
+                  type="text"
+                  value={userID}
+                  placeholder="Email or Phone Number"
+                  onChange={handleOnchangeUserID}
+                  required
+                />
               </div>
 
-              <div className='login_detail'>
+              <div className="login_detail">
                 <FaLock />
-                <input type={showPassword === false ? 'password' : 'text'}
-                  value={password} placeholder='PassWord'
-                  onChange={handleOnchangePassWord} required
+                <input
+                  type={showPassword === false ? "password" : "text"}
+                  value={password}
+                  placeholder="PassWord"
+                  onChange={handleOnchangePassWord}
+                  required
                 />
-                <span className='eyes' onClick={handleShowPass}>
+                <span className="eyes" onClick={handleShowPass}>
                   {showPassword === true ? <FaEyeSlash /> : <IoEyeSharp />}
                 </span>
               </div>
-
             </div>
-            <div className='other'>
+            <div className="other">
               <div>
-                <a href='#'>Register an account</a>
+                <a href="/register">Register an account</a>
               </div>
               <div>
-                <a href='#'>Forget Password?</a>
+                <a href="#">Forget Password?</a>
               </div>
             </div>
 
-            <input type='submit' value='Log In' />
-
-
+            <input type="submit" value="Log In" />
           </form>
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-
-export default Login
+export default Login;
