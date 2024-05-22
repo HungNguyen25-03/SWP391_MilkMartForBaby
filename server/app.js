808f17235f868eabc97a60a14ca85c8af8892e28
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-05-23 00:20:49 +0700

    update server login

--- server/app.js
+++ server/app.js
@@ -1,15 +1,14 @@
-const express=require('express');
-const path=require('path');
+const express = require("express");
+const path = require("path");
 
+const app = express();
 
-const app=express();
+const userRoutes = require("./routes/user");
+app.use(express.json());
 
-const  userRoutes = require("./routes/user");  
+app.use("/api/user", userRoutes);
 
-app.use("/api/user", userRoutes);  
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
-module.exports=app;
+app.listen(4000, () => {
+  console.log("Server is running on port 3000");
+});
+module.exports = app;
--- server/models/userModels.js
+++ server/models/userModels.js
@@ -1,31 +1,26 @@
+const { sql, poolPromise } = require("./database");
+const bcrypt = require("bcrypt");
 
-const {sql, poolPromise}=require('./database');
-const bcrypt = require('bcrypt');
-
-async function loginUser(email,password){
-    try {
-    const pool=await poolPromise;
-    const result=await pool.request() 
-    .input('email', sql.VarChar, email)
-    .query('SELECT * FROM Users WHERE email = @email');
+async function loginUser(email, password) {
+  try {
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query(
+        `SELECT * FROM Users WHERE email = '${email}' AND password = '${password}'`
+      );
     const user = result.recordset[0];
 
-    if(user){
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (isMatch) {
-          return { success: true, user };
-        }
-        }else{
-            return { success:false,message:'Invalid Email or password'};
-
-    } 
-
-    } catch (error) {
-        console.log('Error logging',error);
-        throw error;
-
-    };
-};
+    if (user) {
+      return { success: true, user };
+    } else {
+      return { success: false, message: "Invalid Email or password" };
+    }
+  } catch (error) {
+    // console.log("Error logging", error);
+    throw error;
+  }
+}
 module.exports = {
-    loginUser
-  };
+  loginUser,
+};
