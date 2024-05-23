201bb192f800bec864d5627ef9c038a984e9c22f
thienhung27hao@gmail.com <thienhung27hao@gmail.com>
2024-05-23 13:30:54 +0700

    update filter delete for admin

--- client/src/components/Admin/UserManagement/UserManagement.js
+++ client/src/components/Admin/UserManagement/UserManagement.js
@@ -1,24 +1,14 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../NavBar/NavBar";
 import DataTable from "react-data-table-component";
+import { MdDelete, MdModeEdit } from "react-icons/md";
 import "./UserManagement.scss";
+import Modal from "../Modal/Modal";
 
 export default function UserManagement() {
   const [data, setData] = useState([]);
   const [records, setRecords] = useState(data);
-
-  const column = [
-    {
-      name: "Name",
-      selector: (row) => row.name,
-      sortable: true,
-    },
-    {
-      name: "Role",
-      selector: (row) => row.role,
-      sortable: true,
-    },
-  ];
+  const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     fetch("http://127.0.0.1:1880/user")
@@ -38,6 +28,50 @@ export default function UserManagement() {
     setRecords(newData);
   }
 
+  function handleDelete(id) {
+    const newData = records.filter((record) => record.id !== id);
+    setRecords(newData);
+    setData(newData);
+  }
+
+  function handleSubmit(newRow) {
+    setRecords([...records, newRow]);
+    setData([...data, newRow]);
+  }
+
+  const column = [
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Role",
+      selector: (row) => row.role,
+      sortable: true,
+    },
+    {
+      cell: (row) => (
+        <div className="action">
+          <button
+            onClick={() => {
+              handleDelete(row.id);
+            }}
+          >
+            <MdDelete color="red" />
+          </button>
+          <button
+            onClick={() => {
+              handleDelete(row.id);
+            }}
+          >
+            <MdModeEdit color="green" />
+          </button>
+        </div>
+      ),
+    },
+  ];
+
   return (
     <div className="userManage_container">
       <NavBar />
@@ -49,11 +83,23 @@ export default function UserManagement() {
             <label>Search: </label>
             <input type="text" onChange={handleFilter}></input>
           </div>
-          <div className="delete">
-            <button>Delete User</button>
-          </div>
           <div className="add">
-            <button>Add User</button>
+            <button
+              className="btn"
+              onClick={() => {
+                setModalOpen(true);
+              }}
+            >
+              Add User
+            </button>
+            {modalOpen && (
+              <Modal
+                closeModal={() => {
+                  setModalOpen(false);
+                }}
+                onSubmit={handleSubmit}
+              />
+            )}
           </div>
         </div>
         <div className="table">
