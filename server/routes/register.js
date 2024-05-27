2e7a9fa48ab70c80a854b852ebc19c12ecbf0094
Cong Le Hieu <conglehieu@gmail.com>
2024-05-27 14:06:10 +0700

    modify app.js, register.js and add registerModels.js

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { sql, poolPromise } = require('./database');
 const router = express.Router();
 
 const app = express();
@@ -10,43 +9,21 @@ const port = 3000;
 app.use(bodyParser.json());
 
 // Register a new user
-app.post('/register', async (req, res) => {
+router.post('/register', async (req, res) => {
+  const { username, password, email } = req.body;
+
   try {
-    const { username, password, email } = req.body;
-
-    if (!username || !password || !email) {
-      return res.status(400).json({ message: 'Username and password and email are required' });
+    const user = await registerUser(username, password, email);
+    if (user.success) {
+      res.status(200).json({ message: user.message });
+    } else {
+      res.status(409).json({ message: user.message });
     }
-
-    const pool = await poolPromise;
-
-    // Check if user already exists
-    const userResult = await pool
-      .request()
-      .input('username', sql.VarChar, username)
-      .query(`SELECT * FROM Users WHERE username = '${username}'`);
-
-    if (userResult.recordset.length > 0) {
-      return res.status(400).json({ message: 'Username already exists' });
-    }
-
-    // Insert the new user into the database
-    await pool
-      .request()
-      .input('username', sql.VarChar, username)
-      .input('password', sql.VarChar, password)
-      .input('email', sql.VarChar, email)
-      .query(`INSERT INTO Users (username, password, email, role_id) VALUES 
-      ('${username}', '${password}', '${email}', customer)`);
-
-    res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).send('Error registering user');
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+
 
 module.exports = router;
