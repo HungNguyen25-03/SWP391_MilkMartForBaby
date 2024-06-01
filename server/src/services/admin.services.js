e5aef96f8b4153476e6c6caf8a493db948c5a99a
Cong Le Hieu <conglehieu@gmail.com>
2024-06-01 14:00:17 +0700

    add validation of duplicate username for admin

--- server/src/services/admin.services.js
+++ server/src/services/admin.services.js
@@ -12,28 +12,15 @@ async function createUser(username, password, email, role_id) {
       return { success: false, message: "Username already exists" };
     }
 
-    const result = await pool
-      .request()
-      .input("username", sql.VarChar, username)
-      .input("password", sql.VarChar, password)
-      .input("email", sql.VarChar, email)
-      .input("role_id", sql.VarChar, role_id).query(`
-          INSERT INTO Users (username, password, email, role_id)
-          VALUES (@username, @password, @email, @role_id);
-        `);
+    //insert new user
+    await pool.request().query(
+      `INSERT INTO Users (username, password, email, role_id) VALUES 
+        ('${username}', '${password}', '${email}', '${role_id}' )`
+    );
 
-    if (result.rowsAffected && result.rowsAffected[0] > 0) {
-      return { success: true, message: "User created successfully" };
-    } else {
-      return { success: false, message: "Failed to create user" };
-    }
+    return { success: true, message: "User registered successfully" };
   } catch (error) {
-    console.error("Error creating user:", error);
-    return {
-      success: false,
-      message: "Failed to create user",
-      error: error.message,
-    };
+    throw error;
   }
 }
 
