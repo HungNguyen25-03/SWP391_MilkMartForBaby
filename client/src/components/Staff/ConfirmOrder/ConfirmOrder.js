040667e60c63baee4a2540a2d2791be4c7e3ce02
trizo1104 <vominhtri221104@gmail.com>
2024-06-02 23:12:51 +0700

    update table in staff screen

--- client/src/components/Staff/ConfirmOrder/ConfirmOrder.js
+++ client/src/components/Staff/ConfirmOrder/ConfirmOrder.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import './ConfirmOrder.scss'
 
 export default function ConfirmOrder() {
     return (
-        <div className='confirm'>
-            <table>
-                <tr>
-                    <th>Order ID</th>
-                </tr>
-                <tr>
-                    <td>001</td>
-                </tr>
-            </table>
-        </div>
+        <table>
+            <tr>
+                <th>Order ID</th>
+                <th>Date</th>
+                <th>Customer Name</th>
+                <th>Status</th>
+                <th>Amount</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>2024-06-01</td>
+                <td>John Doe</td>
+                <td>Confirmed</td>
+                <td>$100.00</td>
+                <td><button class="action-btn">View</button></td>
+            </tr>
+        </table>
     )
 }
