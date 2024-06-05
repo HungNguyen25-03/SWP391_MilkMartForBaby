d78cb6d7f68ad6d9ae409f65f6cf885c8c2e18a9
Cong Le Hieu <conglehieu@gmail.com>
2024-06-05 16:31:22 +0700

    add authJWT

--- server/src/middlewares/authJwt.middlewares.js
+++ server/src/middlewares/authJwt.middlewares.js
@@ -1,39 +1,28 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config/auth.config');
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config");
 const { poolPromise, sql } = require("../services/database.services");
 
-verifyToken = (req, res, next) => {
-    let token = req.headers["x-access-token"];
+const verifyToken = async (req, res, next) => {
+    const token = req.headers["x-access-token"];
     
     if (!token) {
-        return res.status(403).send({ message: "No token provided!" });
+        return res.status(403).send({
+        message: "No token provided!",
+        });
     }
     
-    jwt.verify(token, config.secret, (err, decoded) => {
+    jwt.verify(token, config.secret, async (err, decoded) => {
         if (err) {
-        return res.status(401).send({ message: "Unauthorized!" });
+        return res.status(401).send({
+            message: "Unauthorized!",
+        });
         }
+    
         req.userId = decoded.id;
         next();
     });
-};
+}
 
-isAdmin = async (req, res, next) => {
-    const pool = await poolPromise;
-    const result = await pool
-        .request()
-        .input("user_id", sql.Int, req.userId)
-        .query(`SELECT role_id FROM Users WHERE user_id = @user_id`);
-    const user = result.recordset[0];
-    if (user.role_id === 1) {
-        next();
-        return;
-    }
-    return res.status(403).send({ message: "Require Admin Role!" });
-};
-
-const authJwt = {
-    verifyToken: verifyToken,
-    isAdmin: isAdmin,
-};
-module.exports = authJwt;
+module.exports = {
+    verifyToken,
+}
