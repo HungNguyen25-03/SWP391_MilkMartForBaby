d6ed7a60773bb9e28a79069ae469ab4e1bba7508
khai2007 <khaitdse183260@fpt.edu.vn>
2024-06-06 23:35:32 +0700

    BE_Staff get Product

--- server/src/controller/staff.controller.js
+++ server/src/controller/staff.controller.js
@@ -1,4 +1,4 @@
-const {createVoucher,getAllUser} = require('../services/staff.services');
+const {createVoucher,getAllUser, getAllProduct} = require('../services/staff.services');
 
 const createVoucherController = async (req, res) => {
     const {discount, expiration_date} = req.body;
@@ -32,10 +32,25 @@ const getAllUserController = async (req, res) => {
     }
   };
   
+  const getAllProductController = async (req, res) => {
+    try {
+      const result = await getAllProduct();
+  
+      if (result.success) {
+        return res.status(200).json({ user: result.product });
+      } else {
+        return res.status(404).json({ message: result.message });
+      }
+    } catch (error) {
+      console.log("Fail to get all user", error);
+      res.status(500).json({ message: "Error getting all user" });
+    }
+  };
 
-
+ 
 
 module.exports = {
     createVoucherController,
     getAllUserController,
+    getAllProductController
 }
