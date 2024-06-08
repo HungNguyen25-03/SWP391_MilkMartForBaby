3b49c699b797a1096d8647993b7b2958e3726b5f
trizo1104 <vominhtri221104@gmail.com>
2024-06-08 21:45:13 +0700

    update scss for each file

--- client/src/components/Staff/ManageUser/GetManageUser.js
+++ client/src/components/Staff/ManageUser/GetManageUser.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import ManageUser from './ManageUser'
 
-export default function GetManageStaff() {
-
+export default function GetManageUser() {
     const [users, setUsers] = useState()
 
     useEffect(() => {
-        fetch('http://127.0.0.1:1880/managestaff')
+        fetch("http://127.0.0.1:1880/managestaff")
             .then((res) => res.json())
             .then((date) => setUsers(date))
     }, [])
