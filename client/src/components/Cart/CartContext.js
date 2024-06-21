5e0f13cd499a598221f6ed4b4f501b690071d72a
trizo1104 <vominhtri221104@gmail.com>
2024-06-21 08:16:00 +0700

    update CartContext in cart

--- client/src/components/Cart/CartContext.js
+++ client/src/components/Cart/CartContext.js
@@ -3,39 +3,31 @@ import { createContext, useState } from "react";
 const CartContext = createContext();
 
 function CartProvider({ children }) {
-  const [cartList, setCartList] = useState([]);
+    const [cartList, setCartList] = useState([]);
 
-  const handleAddToCart = (product) => {
-    const checkExist = cartList.find(
-      (item) => item.product_id === product.product_id
-    );
-    if (checkExist) {
-      const updatedCart = cartList.map((item) =>
-        item.product_id === product.product_id
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      );
-      setCartList(updatedCart);
-    } else {
-      setCartList([...cartList, { ...product, quantity: 1 }]);
-    }
-  };
+    const handleAddToCart = (product) => {
+        const checkExist = cartList.find(
+            (item) => item.product_id === product.product_id
+        );
+        if (checkExist) {
+            const updatedCart = cartList.map((item) =>
+                item.product_id === product.product_id
+                    ? { ...item, quantity: item.quantity + 1 }
+                    : item
+            );
+            setCartList(updatedCart);
+        } else {
+            setCartList([...cartList, { ...product, quantity: 1 }]);
+        }
+    };
 
-  const handleDeleteCart = (product) => {
-    const updatedCart = cartList.filter(
-      (item) => item.product_id !== product.product_id
-    );
-    setCartList(updatedCart);
-  };
+    const handleDeleteCart = (product) => {
+        const updatedCart = cartList.filter(
+            (item) => item.product_id !== product.product_id
+        );
+        setCartList(updatedCart);
+    };
 
-
-  return (
-    <CartContext.Provider
-      value={{ cartList, handleAddToCart, handleDeleteCart }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
     const incrementQuantity = (product) => {
         setCartList(
             cartList.map(item =>
