f3fc9928c3ecfe98d99d94c2cd9cac7fb318ad0f
khai2007 <khaitdse183260@fpt.edu.vn>
2024-06-22 11:15:56 +0700

    BE rating

--- server/src/services/order.services.js
+++ server/src/services/order.services.js
@@ -1,4 +1,4 @@
-const { poolPromise ,sql} = require("./database.services");
+const { poolPromise, sql } = require("./database.services");
 
 async function getAllOrder() {
   try {
@@ -34,130 +34,131 @@ async function getAllOrder() {
   }
 }
 
-async function getOrderById(order_id){
+async function getOrderById(order_id) {
   try {
-   const pool = await  poolPromise;
-         const result = await pool
-         .request()
-         .query(
-          `SELECT * FROM Products WHERE order_id='${order_id}'`
-         );
-       const order = result.recordset;
-       if(order){
-         return { success: true, order };
-       } else {
-         return { success: false, message: "Fail to connect Order 2" };
-       }
+    const pool = await poolPromise;
+    const result = await pool
+      .request()
+      .query(
+        `SELECT * FROM Products WHERE order_id='${order_id}'`
+      );
+    const order = result.recordset;
+    if (order) {
+      return { success: true, order };
+    } else {
+      return { success: false, message: "Fail to connect Order 2" };
+    }
   } catch (error) {
-   throw error;
+    throw error;
   }
- };
+};
 
- async function getCompleteStatus(){
+async function getCompleteStatus() {
   try {
-   const pool = await  poolPromise;
-         const request = await pool.request();
+    const pool = await poolPromise;
+    const request = await pool.request();
 
-         request.input('status', sql.NVarChar, 'Complete');
-       
-       
-         const query =
-          `SELECT * FROM Orders WHERE status=@status`
-         ;
-          const result=await request.query(query);
-         const order = result.recordset;
-       if(order){
-         return { success: true, order };
-       } else {
-         return { success: false, message: "Fail to connect Order Complete Status" };
-       }
+    request.input('status', sql.NVarChar, 'Complete');
+
+
+    const query =
+      `SELECT * FROM Orders WHERE status=@status`
+      ;
+    const result = await request.query(query);
+    const order = result.recordset;
+    if (order) {
+      return { success: true, order };
+    } else {
+      return { success: false, message: "Fail to connect Order Complete Status" };
+    }
   } catch (error) {
-   throw error;
+    throw error;
   }
- };
+};
 
- 
 
- async function getPendingStatus(){
+
+async function getPendingStatus() {
   try {
-   const pool = await  poolPromise;
-         const request = await pool.request();
+    const pool = await poolPromise;
+    const request = await pool.request();
 
-         request.input('status', sql.NVarChar, 'Pending');
-       
-       
-         const query =
-          `SELECT * FROM Orders WHERE status=@status`
-         ;
-          const result=await request.query(query);
-         const order = result.recordset;
-       if(order){
-         return { success: true, order };
-       } else {
-         return { success: false, message: "Fail to connect Order Pending Status" };
-       }
+    request.input('status', sql.NVarChar, 'Pending');
+
+
+    const query =
+      `SELECT * FROM Orders WHERE status=@status`
+      ;
+    const result = await request.query(query);
+    const order = result.recordset;
+    if (order) {
+      return { success: true, order };
+    } else {
+      return { success: false, message: "Fail to connect Order Pending Status" };
+    }
   } catch (error) {
-   throw error;
+    throw error;
   }
- };
+};
 
- async function getConfirmStatus(){
+async function getConfirmStatus() {
   try {
-   const pool = await  poolPromise;
-         const request = await pool.request();
+    const pool = await poolPromise;
+    const request = await pool.request();
 
-         request.input('status', sql.NVarChar, 'Confirmed');
-       
-       
-         const query =
-          `SELECT * FROM Orders WHERE status=@status`
-         ;
-          const result=await request.query(query);
-         const order = result.recordset;
-       if(order){
-         return { success: true, order };
-       } else {
-         return { success: false, message: "Fail to connect Order Confirmed Status" };
-       }
+    request.input('status', sql.NVarChar, 'Confirmed');
+
+
+    const query =
+      `SELECT * FROM Orders WHERE status=@status`
+      ;
+    const result = await request.query(query);
+    const order = result.recordset;
+    if (order) {
+      return { success: true, order };
+    } else {
+      return { success: false, message: "Fail to connect Order Confirmed Status" };
+    }
   } catch (error) {
-   throw error;
+    throw error;
   }
- };
+};
 
 
 
- async function getDeliverStatus(){
+async function getDeliverStatus() {
   try {
-   const pool = await  poolPromise;
-         const request = await pool.request();
+    const pool = await poolPromise;
+    const request = await pool.request();
 
-         request.input('status', sql.NVarChar, 'Delivered');
-       
-       
-         const query =
-          `SELECT * FROM Orders WHERE status=@status`
-         ;
-          const result=await request.query(query);
-         const order = result.recordset;
-       if(order){
-         return { success: true, order };
-       } else {
-         return { success: false, message: "Fail to connect Order Delivered Status" };
-       }
+    request.input('status', sql.NVarChar, 'Delivered');
+
+
+    const query =
+      `SELECT * FROM Orders WHERE status=@status`
+      ;
+    const result = await request.query(query);
+    const order = result.recordset;
+    if (order) {
+      return { success: true, order };
+    } else {
+      return { success: false, message: "Fail to connect Order Delivered Status" };
+    }
   } catch (error) {
-   throw error;
+    throw error;
   }
- };
+};
 
 
 
- module.exports = {
-    getAllOrder,
-    getOrderById,
-    getCompleteStatus,
-    getPendingStatus,
-    getConfirmStatus,
-    getDeliverStatus
-    
-  };
-  
+module.exports = {
+  getAllOrder,
+  getOrderById,
+  getCompleteStatus,
+  getPendingStatus,
+  getConfirmStatus,
+  getDeliverStatus,
+
+
+};
+
