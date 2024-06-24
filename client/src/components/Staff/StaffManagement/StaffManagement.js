3c3b83c57b08f2c7b4f90f6df2f6b8f2898a7ed8
trizo1104 <vominhtri221104@gmail.com>
2024-06-24 11:42:23 +0700

    update staff screen

--- client/src/components/Staff/StaffManagement/StaffManagement.js
+++ client/src/components/Staff/StaffManagement/StaffManagement.js
@@ -1,34 +1,7 @@
-// import React from 'react'
-// import NavBarStaff from '../NavBar/NavBarStaff'
-// import { Route, Routes } from 'react-router-dom'
-// import GetDetailConfirm from '../ConfirmOrder/GetDetailConfirm'
-// import GetInentory from '../ManageInventory/GetInventory'
-// import GetVoucher from '../CreateVoucher/GetVoucher'
-// import GetTrackOrder from '../TrackOrder/GetTrackOrder'
-// import GetManageUser from '../ManageUser/GetManageUser'
-
-// export default function StaffManagement() {
-//     return (
-//         <div className='row'>
-//             <NavBarStaff />
-//             <Routes>
-//                 <Route path='/comfirm_order' element={<GetDetailConfirm />}></Route>
-//                 <Route path='/manage_inventory' element={<GetInentory />}></Route>
-//                 <Route path='/manage_users' element={<GetManageUser />}></Route>
-//                 <Route path='/create_voucher_codes' element={<GetVoucher />}></Route>
-//                 <Route path='/track_orders' element={<GetTrackOrder />}></Route>
-//                 {/*<Route path='/comfirm_order' element={<ConfirmOrder />}></Route> */}
-//             </Routes>
-//         </div>
-//     )
-// }
-
-
 import React from 'react';
 import NavBarStaff from '../NavBar/NavBarStaff';
 import { Route, Routes } from 'react-router-dom';
-import GetDetailConfirm from '../ConfirmOrder/GetDetailConfirm';
-import GetInventory from '../ManageInventory/GetInventory'; // Corrected the import name
+import GetInventory from '../ManageInventory/GetInventory';
 import GetVoucher from '../CreateVoucher/GetVoucher';
 import GetTrackOrder from '../TrackOrder/GetTrackOrder';
 import GetManageUser from '../ManageUser/GetManageUser';
