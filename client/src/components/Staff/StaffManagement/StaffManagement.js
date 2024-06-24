b5b7ee695cd15c748ed47de89301eb7873e03e0c
trizo1104 <vominhtri221104@gmail.com>
2024-06-24 21:28:49 +0700

    update CRUD in staff at inventory

--- client/src/components/Staff/StaffManagement/StaffManagement.js
+++ client/src/components/Staff/StaffManagement/StaffManagement.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import NavBarStaff from '../NavBar/NavBarStaff';
 import { Route, Routes } from 'react-router-dom';
-import GetInventory from '../ManageInventory/GetInventory';
-import GetVoucher from '../CreateVoucher/GetVoucher';
 import GetTrackOrder from '../TrackOrder/GetTrackOrder';
 import GetManageUser from '../ManageUser/GetManageUser';
 import GetManagePosts from '../ManagePosts/GetManagePosts';
 import ConfirmOrder from '../ConfirmOrder/ConfirmOrder';
+import ManageVoucher from '../CreateVoucher/ManageVoucher';
+import ManageInventory from '../ManageInventory/ManageInventory';
 
 export default function StaffManagement() {
     return (
@@ -18,9 +18,9 @@ export default function StaffManagement() {
                 <div className="col-md-10 col-sm-9">
                     <Routes>
                         <Route path='/comfirm_order' element={<ConfirmOrder />} />
-                        <Route path='/manage_inventory' element={<GetInventory />} />
+                        <Route path='/manage_inventory' element={<ManageInventory />} />
                         <Route path='/manage_users' element={<GetManageUser />} />
-                        <Route path='/create_voucher_codes' element={<GetVoucher />} />
+                        <Route path='/create_voucher_codes' element={<ManageVoucher />} />
                         {/* <Route path='/track_orders' element={<GetTrackOrder />} />
                         <Route path='/manage_posts' element={<GetManagePosts />} /> */}
                     </Routes>
