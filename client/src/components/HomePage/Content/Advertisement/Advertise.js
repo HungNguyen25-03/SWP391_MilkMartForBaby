a6666dff106c742a0053a890218f39f81dcaf1ca
trizo1104 <vominhtri221104@gmail.com>
2024-07-23 11:43:14 +0700

    update fix class to className in advertise

--- client/src/components/HomePage/Content/Advertisement/Advertise.js
+++ client/src/components/HomePage/Content/Advertisement/Advertise.js
@@ -4,15 +4,15 @@ import "./Advertise.scss";
 export default function Advertise() {
   return (
     <div className="ad">
-      <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="3000">
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="3000">
             <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/swp391-milkmartsystem.appspot.com/o/images%2Fadvertise%2Fqc.png?alt=media&token=b59fbf4e-db19-4f61-a1a7-36bc8257863e" alt="advertisement" />
           </div>
-          <div class="carousel-item" data-bs-interval="3000">
+          <div className="carousel-item" data-bs-interval="3000">
             <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/swp391-milkmartsystem.appspot.com/o/images%2Fadvertise%2Fad2.png?alt=media&token=4d5eee70-1cd0-4cb8-a806-0bc18129f878" alt="advertisement" />
           </div>
-          <div class="carousel-item" data-bs-interval="3000">
+          <div className="carousel-item" data-bs-interval="3000">
             <img className="d-block w-100" src="https://firebasestorage.googleapis.com/v0/b/swp391-milkmartsystem.appspot.com/o/images%2Fadvertise%2Fad4.png?alt=media&token=971a2b04-ef3d-44c2-9511-1f9f671e5950" alt="advertisement" />
           </div>
           <div className="carousel-item" data-bs-interval="3000">
